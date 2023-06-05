6fd3086a2d582b8dfa59a02cc47d8a1a78dc1adb
skeleton2010 <inseong747@gmail.com>
2023-06-05 10:24:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# team-skeleton2010-homepage - copy
---
+++ dist/src/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+app.set('views', path_1.default.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res, next) => {
+    res.render('home/index.ejs');
+});
+app.get('/privacy', (req, res, next) => {
+    res.render('home/privacy.ejs');
+});
+app.listen('80', () => {
+    console.log('listening on http://localhost');
+});
